4921ce694fc87a8617a086152b9b0a8104d73e83
Gerald Gicharu <gicharugerald@gmail.com>
2023-09-19 00:24:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+
+// Multiply to result to 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+// calulating remainder that equals to 4
+let num3 = 34;
+let num4 = 10;
+let mod = num3 % num4;
+
+// return 20 as highest number in set
+let numberArray = [7, 11, 13, 17, 20]
+let max = Math.max(...numberArray)
+
+// integer greater than 0
+let random = Math.floor(Math.random() * 100) +1;
